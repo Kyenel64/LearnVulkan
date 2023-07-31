06172772dbda6b4866bfbee86fbb2137c6796a5d
kyenel64 <kyenelson3@gmail.com>
2023-07-31 15:20:04 -0700

    Create vulkan instance

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 #include <cstdlib>
 
 const uint32_t WIDTH = 800;
@@ -33,7 +34,7 @@ private:
 
     void InitVulkan()
     {
-
+        CreateInstance();
     }
 
     void MainLoop()
@@ -46,12 +47,58 @@ private:
 
     void Cleanup()
     {
+        vkDestroyInstance(m_VulkanInstance, nullptr);
         glfwDestroyWindow(m_Window);
         glfwTerminate();
     }
 
+    void CreateInstance()
+    {
+        // Vulkan object creation function parameters pattern:
+        // 1. Pointer to struct with creation info
+        // 2. Pointer to custom allocator callbacks, always nullptr in this tutorial
+        // 3. Pointer to the variable that stores the handle to the new object
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Learn Vulkan";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        // Create instance
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &m_VulkanInstance);
+        if (result != VK_SUCCESS)
+            throw std::runtime_error("failed to create instance!");
+
+        // Check extensions
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        
+        std::cout << "Available extensions: \n";
+        for (const auto& extension : extensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+    }
+
 private:
     GLFWwindow* m_Window;
+
+    VkInstance m_VulkanInstance;
 };
 
 int main() {
